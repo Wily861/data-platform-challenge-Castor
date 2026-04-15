b6272f38a6bf09b69459b22be5dcfc3177f51f98
Wily Duvan Villamil Rey <willyvillamil861@gmail.com>
2026-04-15 13:39:18 -0500

    Update 03_design_and_bi.sql

--- sql/03_design_and_bi.sql
+++ sql/03_design_and_bi.sql
@@ -3,15 +3,15 @@
 -- ==========================================================
 
 -- Vista Materializada (Punto 2.1)
--- Pre-agregación de métricas mensuales para Power BI[cite: 22].
+-- Pre-agregación de métricas mensuales para Power BI.
 CREATE MATERIALIZED VIEW mv_bi_monthly_sales AS
 SELECT product_id, DATE_TRUNC('month', sale_date) AS month, SUM(total_amount) AS revenue
 FROM fact_sales GROUP BY 1, 2;
 
--- Índice único para permitir REFRESH CONCURRENTLY sin bloquear Power BI[cite: 16, 22].
+-- Índice único para permitir REFRESH CONCURRENTLY sin bloquear Power BI.
 CREATE UNIQUE INDEX idx_mv_monthly_sales_unique ON mv_bi_monthly_sales (product_id, month);
 
 -- Estrategia de Storage (Punto 2.2)
--- Separación física de datos para optimizar I/O[cite: 23].
+-- Separación física de datos para optimizar I/O.
 -- TABLESPACE staging_ts LOCATION '/data/standard_ssd'; -- Volátil
 -- TABLESPACE core_ts LOCATION '/data/premium_nvme';    -- Persistente/Lectura BI
